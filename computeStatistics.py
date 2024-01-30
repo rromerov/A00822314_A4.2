d1438f91cfe51f0af885829a8d4c24c143591f5e
Roberto Romero Vielma <A00822314@tec.mx>
2024-01-30 13:20:46 -0600

    Add execution time to statistics results

--- computeStatistics.py
+++ computeStatistics.py
@@ -1,21 +1,25 @@
+import time
 from utilities.dataLoader import txtLoader
 from utilities.statistics import statistics
 
+start_time = time.time()
+
 numbers = txtLoader()
+
 measures = statistics(numbers)
 
-print(f'Mean: {measures.mean()}\n'
-      f'Median: {measures.median()}\n'
-      f'Mode: {measures.mode()}\n'
-      f'Standard deviation: {measures.standard_deviation()}\n'
-      f'Variance: {measures.variance()}')
+end_time = time.time()
 
+results = (f'Mean: {measures.mean()}\n'
+           f'Median: {measures.median()}\n'
+           f'Mode: {measures.mode()}\n'
+           f'Standard deviation: {measures.standard_deviation()}\n'
+           f'Variance: {measures.variance()}\n'
+           f'Execution time: {end_time - start_time} seconds')
+
+print(results)
 
 with open('StatisticsResults.txt', 'w') as output:
-    print(f'Mean: {measures.mean()}\n'
-          f'Median: {measures.median()}\n'
-          f'Mode: {measures.mode()}\n'
-          f'Standard deviation: {measures.standard_deviation()}\n'
-          f'Variance: {measures.variance()}', file=output)
+    print(results, file=output)
 
 
