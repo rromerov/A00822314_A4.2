73f67ffe5434659f43a549d72c842afa0d2aec20
Roberto Romero Vielma <A00822314@tec.mx>
2024-01-30 12:45:16 -0600

    Refactor computeStatistics.py to consolidate print statements and write results to file

--- computeStatistics.py
+++ computeStatistics.py
@@ -4,16 +4,18 @@ from utilities.statistics import statistics
 numbers = txtLoader()
 measures = statistics(numbers)
 
-print(f'Mean: {measures.mean()}')
-print(f'Median: {measures.median()}')
-print(f'Mode: {measures.mode()}')
-print(f'Standard deviation: {measures.standard_deviation()}')
-print(f'Variance: {measures.variance()}')
+print(f'Mean: {measures.mean()}\n'
+      f'Median: {measures.median()}\n'
+      f'Mode: {measures.mode()}\n'
+      f'Standard deviation: {measures.standard_deviation()}\n'
+      f'Variance: {measures.variance()}')
+
 
 with open('StatisticsResults.txt', 'w') as output:
-    print(f'Mean: {measures.mean()}', file = output)
-    print(f'Median: {measures.median()}', file = output)
-    print(f'Mode: {measures.mode()}', file = output)
-    print(f'Standard deviation: {measures.standard_deviation()}', file = output)
-    print(f'Variance: {measures.variance()}', file = output)
+    print(f'Mean: {measures.mean()}\n'
+          f'Median: {measures.median()}\n'
+          f'Mode: {measures.mode()}\n'
+          f'Standard deviation: {measures.standard_deviation()}\n'
+          f'Variance: {measures.variance()}', file=output)
+
 
