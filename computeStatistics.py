88b6747be925a02e54ac9d1a981d03d23767045e
Roberto Romero Vielma <A00822314@tec.mx>
2024-01-30 12:35:00 -0600

    Update statistics calculations and results

--- computeStatistics.py
+++ computeStatistics.py
@@ -6,12 +6,14 @@ measures = statistics(numbers)
 
 print(f'Mean: {measures.mean()}')
 print(f'Median: {measures.median()}')
+print(f'Mode: {measures.mode()}')
 print(f'Standard deviation: {measures.standard_deviation()}')
 print(f'Variance: {measures.variance()}')
 
 with open('StatisticsResults.txt', 'w') as output:
     print(f'Mean: {measures.mean()}', file = output)
     print(f'Median: {measures.median()}', file = output)
+    print(f'Mode: {measures.mode()}', file = output)
     print(f'Standard deviation: {measures.standard_deviation()}', file = output)
     print(f'Variance: {measures.variance()}', file = output)
 
--- utilities/statistics.py
+++ utilities/statistics.py
@@ -1,4 +1,5 @@
 class statistics:
+
     def __init__(self, data):
         self.data = data
     
@@ -12,6 +13,20 @@ class statistics:
             return (sortedData[n//2 - 1] + sortedData[n//2]) / 2
         else:
             return sortedData[n//2]
+
+    def mode(self):
+        counts = {}
+        for x in self.data:
+            if x in counts:
+                counts[x] += 1
+            else:
+                counts[x] = 1
+        if counts:
+            mode_value = max(counts, key=counts.get)
+            if list(counts.values()).count(counts[mode_value]) == 1:
+                return mode_value
+            else:
+                return "There is no mode"
         
     def standard_deviation(self):
         mean = self.mean()
