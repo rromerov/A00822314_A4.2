ad381b01897bb9dbac3c019a1b05597fb22c30ca
Roberto Romero Vielma <A00822314@tec.mx>
2024-01-31 16:34:56 -0600

    Rename output file for convert_numbers

--- convert_numbers.py
+++ convert_numbers.py
@@ -30,6 +30,6 @@ print(results)
 execution_time = time.time() - start_time
 print(f'Execution time: {execution_time:.2f} seconds')
 
-with open('StatisticsResults.txt', 'w', encoding='utf-8') as output:
+with open('ConvertionResults.txt', 'w', encoding='utf-8') as output:
     output_content = f'{results}\nExecution time: {execution_time:.2f} seconds'
     output.write(output_content)
