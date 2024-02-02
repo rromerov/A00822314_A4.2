d710157e85dae74d3f3aaf98d25223fd6225ceee
Roberto Romero Vielma <A00822314@tec.mx>
2024-02-02 00:15:57 -0600

    Refactor module documentation

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # pylint: disable=invalid-name
-"""This module contains various functions and classes that are used throughout
-the application. It includes functionality for calculating statistics, converting
- numbers, loading data, counting words, and more. Each submodule handles a specific
- functionality.
+"""This module contains various functions and classes that are used
+throughout the application. It includes functionality for calculating
+ statistics, converting numbers, loading data, counting words, and more.
+ Each submodule handles a specific functionality.
  """
